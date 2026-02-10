17b0f1b8fd2e64071109fd30141d34e55c78b34e
Christos8369 <161146395+Christos8369@users.noreply.github.com>
2026-02-10 17:50:26 +0200

    Add initial db and api package classes

---
+++ src/main/java/com/mycompany/wikiviewerapp/gui/exampleClass1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.wikiviewerapp.gui;
+
+/**
+ *
+ * @author andri
+ */
+public class exampleClass1 {
+    
+}
