b20b017a4098c8924835b12baea29966913b26c0
Thomas Demisarlis <124501780+tdemis@users.noreply.github.com>
2026-03-03 20:41:26 +0200

    splash

--- WikiViewer/src/view/SplashForm.java
+++ WikiViewer/src/view/SplashForm.java
@@ -9,69 +9,57 @@ public class SplashForm extends javax.swing.JFrame {
         public SplashForm() {
                 initComponents();
 
-                // Προσθήκη περιγράμματος για να φαίνεται ωραίο
-                getRootPane().setBorder(
-                                javax.swing.BorderFactory.createLineBorder(new java.awt.Color(100, 100, 100), 2));
-
-                // Φόρτωση, Σμίκρυνση (Scaling) και Τοποθέτηση της εικόνας ως Background
-                // (600x337 pixels)
-                try {
-                        java.net.URL imgURL = getClass().getResource("/resources/splash.jpg");
-                        if (imgURL != null) {
-                                javax.swing.ImageIcon originalIcon = new javax.swing.ImageIcon(imgURL);
-                                java.awt.Image originalImage = originalIcon.getImage();
-
-                                // Κάνουμε σμίκρυνση διατηρώντας το Aspect Ratio (π.χ. σε 600 πλάτος, 337 ύψος
-                                // που είναι περίπου 16:9)
-                                java.awt.Image scaledImage = originalImage.getScaledInstance(600, 337,
-                                                java.awt.Image.SCALE_SMOOTH);
-                                javax.swing.ImageIcon scaledIcon = new javax.swing.ImageIcon(scaledImage);
-
-                                jLabel1.setIcon(scaledIcon);
-                        }
-                } catch (Exception e) {
-                }
-
-                // Προσθήκη εικονιδίου στην taskbar (αν βρεθεί)
-                try {
-                        java.awt.Image pageIcon = new javax.swing.ImageIcon(
-                                        getClass().getResource("/resources/wikilogo.png")).getImage();
-                        setIconImage(pageIcon);
-                } catch (Exception e) {
-                }
         }
 
         @SuppressWarnings("unchecked")
         // <editor-fold defaultstate="collapsed" desc="Generated
-        // Code">//GEN-BEGIN:initComponents
-        private void initComponents() {
+        // <editor-fold defaultstate="collapsed" desc="Generated
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
 
-                jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
 
-                setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-                setUndecorated(true);
+        jLabel4.setText("jLabel4");
 
-                jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-                jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-                jLabel1.setText("<html><center><h1 style=\"color: white;\">WikiViewer</h1><h3 style=\"color: white;\">Η εγκυκλοπαίδεια στον υπολογιστή σας</h3><br><br><i style=\"color: white;\">Φόρτωση δεδομένων...</i></center></html>");
-                jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setIconImages(null);
+        setUndecorated(true);
+        getContentPane().setLayout(null);
 
-                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-                getContentPane().setLayout(layout);
-                layout.setHorizontalGroup(
-                                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 600,
-                                                                Short.MAX_VALUE));
-                layout.setVerticalGroup(
-                                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 337,
-                                                                Short.MAX_VALUE));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("<html><center><h1 style=\"color: white; margin-bottom: 0px;\">WikiViewer</h1><h3 style=\"color: white; margin-top: 5px;\">Η εγκυκλοπαίδεια στον υπολογιστή σας</h3></center></html> ");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(10, 10, 580, 70);
 
-                pack();
-                setLocationRelativeTo(null);
-        }// </editor-fold>//GEN-END:initComponents
+        jLabel2.setText("<html><ul style=\"color: white; font-size: 12px; font-weight: bold;\"><li>Ανδριόπουλος Χρήστος</li><li>Καραγιάννης Ιωάννης</li><li>Δεμισαρλής Θωμάς</li></ul></html> ");
+        jLabel2.setToolTipText("");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(-10, 190, 300, 110);
 
-        // Variables declaration - do not modify//GEN-BEGIN:variables
-        private javax.swing.JLabel jLabel1;
-        // End of variables declaration//GEN-END:variables
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("<html><i style=\"color: white;\">Φόρτωση δεδομένων...</i></html> ");
+        jLabel3.setToolTipText("");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(10, 300, 580, 16);
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Resources/splash.jpg"))); // NOI18N
+        jLabel6.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(0, 0, 600, 340);
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel6;
+    // End of variables declaration//GEN-END:variables
 }
